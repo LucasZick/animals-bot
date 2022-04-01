3f93d65bedc0426e257270fbd0426044ef8ba0ee
Lucas Alexandre Zick <lucasalexandrezick@gmail.com>
2022-04-01 15:01:07 -0300

    a

--- getAnimal.py
+++ getAnimal.py
@@ -4,10 +4,17 @@ from webbrowser import get
 
 def getRandomAnimal():
     arquivo = open('animals.txt', 'r', encoding="utf8")
+    prefixos = ['Tu é bonito igual um(a) ', 'Tu é inteligente igual um(a) ', 'Tu é chato igual um(a) ', 'Pelo menos, diferente de você, eu não sou um(a) ', 'Você me parece um(a) ', 'Sua aparência me lembra um(a) ', 'É difícil ser assim? Um(a) ']
     animals = []
+    animalsReduct = []
     for linha in arquivo:
         linha = arquivo.readline()
         animals.append(linha)
     arquivo.close()
+    for animal in animals:
+        animalReduct = animal.split('(')
+        animalsReduct.append(animalReduct[0])
 
-    return('Tu é inteligente igual um(a) ' + random.choice(animals))
+    return(random.choice(prefixos) + random.choice(animalsReduct))
+
+print(getRandomAnimal())
