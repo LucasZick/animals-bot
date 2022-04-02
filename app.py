f763c2538364d8d110ceff0cdb6d0e0efe129192
Caio <caiowzy@Caios-iMac.local>
2022-04-02 14:48:57 -0300

    Makes it faster to generate automated messages, read animals list once

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import os
 import tweepy
 from redis import Redis
 
-from getAnimal import get_random_animal
+from MessageBuilders import RandomAnimalMessageBuilder
 
 
 def get_redis(redis_url):
-    instance = Redis.from_url(redis_url, ssl_cert_reqs=None)
+    instance = Redis.from_url(redis_url, ssl_cert_reqs=False)
 
     if not instance.ping():
         raise ConnectionError('Redis instance is down.')
@@ -21,19 +21,28 @@ def get_twitter(api_key, api_secret_key, access_key, access_secret):
 
     return tweepy.API(auth, wait_on_rate_limit=True)
 
+def get_animals():
+    with open('animals.txt', 'r', encoding="utf8") as file:
+        return list(map(lambda animal: animal.split('(')[0].strip(), file.readlines()))
+
 
 def _main_():
     r = get_redis(os.environ['REDIS_URL'])
-    api = get_twitter(os.environ['API_KEY'], os.environ['API_SECRET_KEY'], os.environ['ACESS_KEY'], os.environ['ACCESS_SECRET'])
+    api = get_twitter(os.environ['API_KEY'], os.environ['API_SECRET_KEY'], os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
+
+    animals = get_animals()
+    RandomAnimalMessageBuilder.add_animals(animals)
 
     last_seen_tweet_id = str(r.get('last_seen_tweet_id'))
-    tweets = api.mentions_timeline(last_seen_tweet_id, tweet_mode='extended')
+    tweets = api.mentions_timeline(since_id=last_seen_tweet_id)
+
     print('deu bao')
     print('Ultimo ID pesquisado:' + last_seen_tweet_id)
+
     for tweet in reversed(tweets):
-        r.set('last_seen_id', tweet.id)
-        genius_phrase = get_random_animal()
-        api.update_status(f"@{tweet.user.screen_name} {genius_phrase}", in_reply_to_status_id=tweet.id)
+        random_animal_message = RandomAnimalMessageBuilder.build_random_animal_message()
+        r.set('last_seen_tweet_id', tweet.id)
+        api.update_status(f"@{tweet.user.screen_name} {random_animal_message}", in_reply_to_status_id=tweet.id)
 
 
 while True:
