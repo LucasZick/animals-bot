e467565b1b5b70c7631e734be641dce8c592c001
Zick <lucasalexandrezick@gmail.com>
2023-02-23 19:43:13 +0000

    refac: structure changes and double quotes everywhere

--- twitterBot/MessageBuilders.py
+++ twitterBot/MessageBuilders.py
@@ -6,13 +6,13 @@ class RandomAnimalMessageBuilder:
     __animals__ = []
 
     __prefixos__ = [
-        'Tu é bonito igual um(a)',
-        'Tu é inteligente igual um(a)',
-        'Tu é chato igual um(a)',
-        'Pelo menos, diferente de você, eu não sou um(a)',
-        'Você me parece um(a)',
-        'Sua aparência me lembra um(a)',
-        'É difícil ser assim? Um(a)'
+        "Tu é bonito igual um(a)",
+        "Tu é inteligente igual um(a)",
+        "Tu é chato igual um(a)",
+        "Pelo menos, diferente de você, eu não sou um(a)",
+        "Você me parece um(a)",
+        "Sua aparência me lembra um(a)",
+        "É difícil ser assim? Um(a)"
     ]
 
     @staticmethod
--- twitterBot/__main__.py
+++ twitterBot/__main__.py
@@ -6,12 +6,12 @@ import twitterBot.bot as Bot
 
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
+@app.route("/health", methods=["GET"])
 def health():
     return "I'm still alive!"
 
 def start():
     Process(target=Bot.run).start()
-    app.run(host='0.0.0.0', port=int(CONFIG.PORT))
+    app.run(host="0.0.0.0", port=int(CONFIG.PORT))
 
 start()
