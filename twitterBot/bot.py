a3c590a54f35f43fc1ffdb6f89e3d85b5cc572ea
Zick <lucasalexandrezick@gmail.com>
2023-02-23 19:43:51 +0000

    refac: fixed the first loop when theres no tweet registered

--- twitterBot/bot.py
+++ twitterBot/bot.py
@@ -11,13 +11,13 @@ from twitterBot.config import CONFIG
 
 
 print(CONFIG.LOGLEVEL)
-logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=CONFIG.LOGLEVEL.upper())
+logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=CONFIG.LOGLEVEL.upper())
 
 def get_redis(redis_url):
-    instance = Redis.from_url(redis_url, ssl_cert_reqs=False)
+    instance = Redis.from_url(redis_url)
 
     if not instance.ping():
-        raise ConnectionError('Redis instance is down.')
+        raise ConnectionError("Redis instance is down.")
 
     return instance
 
@@ -28,21 +28,32 @@ def get_twitter(api_key, api_secret_key, access_key, access_secret):
     return tweepy.API(auth, wait_on_rate_limit=True)
 
 def get_animals():
-    with open('animals.txt', 'r', encoding="utf8") as file:
-        return list(map(lambda animal: animal.split('(')[0].strip(), file.readlines()))
+    with open("animals.txt", "r", encoding="utf8") as file:
+        return list(map(lambda animal: animal.split("(")[0].strip(), file.readlines()))
+
+def set_last_tweet(twitter_client, redis_client):
+    tweet = twitter_client.mentions_timeline(count = 1)[0]
+    redis_client.set("last_seen_tweet_id", tweet.id)
 
 def main():
     redis_client = get_redis(CONFIG.REDIS_URL)
-    logging.info('Connected to Redis!')
+    logging.info("Connected to Redis!")
     
-    twitter_client = get_twitter(CONFIG.TWITTER['API_KEY'], CONFIG.TWITTER['API_SECRET_KEY'], CONFIG.TWITTER['ACCESS_KEY'], CONFIG.TWITTER['ACCESS_SECRET'])
-    logging.info('Connected to Twitter!')
+    twitter_client = get_twitter(CONFIG.TWITTER["API_KEY"], CONFIG.TWITTER["API_SECRET_KEY"], CONFIG.TWITTER["ACCESS_KEY"], CONFIG.TWITTER["ACCESS_SECRET"])
+    logging.info("Connected to Twitter!")
 
     animals = get_animals()
     RandomAnimalMessageBuilder.add_animals(animals)
 
     while True:
-        last_seen_tweet_id = int(redis_client.get('last_seen_tweet_id'))
+        last_seen_tweet_id = redis_client.get("last_seen_tweet_id")
+
+        if type(last_seen_tweet_id) == bytes:
+            last_seen_tweet_id = int(last_seen_tweet_id.decode("utf-8")) #transform from bytes to string to, finally, integer
+        else:
+            set_last_tweet(twitter_client, redis_client)
+            continue
+
         logging.info(f"Starting from Tweet ID: {last_seen_tweet_id}.")
 
         tweets = twitter_client.mentions_timeline(since_id=last_seen_tweet_id)
@@ -50,13 +61,13 @@ def main():
 
         for tweet in reversed(tweets):
             try:
-                logging.debug('Got tweet ID {tweet.id}.')
+                logging.debug("Got tweet ID {tweet.id}.")
                 random_animal_message = RandomAnimalMessageBuilder.build_random_animal_message()
                 twitter_client.update_status(f"@{tweet.user.screen_name} {random_animal_message}", in_reply_to_status_id=tweet.id)
-            except BaseException as e:
+            except BaseException:
                 logging.exception(f"Error while trying to reply to tweet ID {tweet.id}.")
             finally:
-                redis_client.set('last_seen_tweet_id', tweet.id)
+                redis_client.set("last_seen_tweet_id", tweet.id)
 
         time.sleep(int(CONFIG.LOOKUP_TIMEOUT))
 
