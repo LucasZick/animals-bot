e415870a2a1b04814db47c585799bf2f63833514
Lucas Zick <lucasalexandrezick@gmail.com>
2023-03-07 20:16:12 +0100

    refac: alterado padrão redis env e info de logging

--- animals_bot/config.py
+++ animals_bot/config.py
@@ -9,10 +9,11 @@ class CONFIG:
         "ACCESS_SECRET": environ.get("ACCESS_SECRET", ""),
     }
 
-    REDIS_URL = environ.get("REDIS_URL", "")
+    REDIS_URL = environ.get("REDIS_URL", "redis://172.17.0.1:6379")
 
     PORT = environ.get("PORT", 3030)
 
     LOOKUP_TIMEOUT = environ.get("LOOKUP_TIMEOUT", 30)
 
+    # NOTSET - DEBUG - INFO - WARN - ERROR - CRITICAL
     LOGLEVEL = environ.get("LOGLEVEL", "INFO")
